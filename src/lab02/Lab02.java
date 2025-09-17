d361e2df56fc5cf179c62ffe86fb0a5406860bd9
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-09-17 09:49:11 -0400

    Finished step 2

--- src/lab02/Lab02.java
+++ src/lab02/Lab02.java
@@ -5,6 +5,8 @@
 package lab02;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 /**
@@ -12,7 +14,9 @@ import javafx.stage.Stage;
  * @author 6324569
  */
 public class Lab02 extends Application {
-
+    BorderPane borderPane = new BorderPane();
+    Scene scene = new Scene(borderPane, 250, 300);
+    
     /**
      * @param args the command line arguments
      */
@@ -22,7 +26,8 @@ public class Lab02 extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Lab01");
+        stage.setTitle("Java Games");
+        stage.setScene(scene);
         stage.show();
     }
 }

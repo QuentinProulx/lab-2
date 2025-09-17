1f2f53f087553aef7ba752f946736407ff452c4c
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-09-17 09:45:30 -0400

    Set up JavaFX

---
+++ src/lab02/Lab02.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab02;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6324569
+ */
+public class Lab02 extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Lab01");
+        stage.show();
+    }
+}
